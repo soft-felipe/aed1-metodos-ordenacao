f60d8402f5d92716b3404f78b3f84c6348f9244f
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2023-01-06 17:37:44 -0300

    Gerad vetores povoados com inteiros aleatorios

---
+++ geradorDeVetor.c
@@ -0,0 +1,22 @@
+/*
+ * Alunos:
+ *  Felipe Moreira - 202105027
+ *  Daniel Nogueira - 202105024
+ *  Norton Almeida - 202203526
+ */
+
+#include <stdlib.h>
+
+/**
+ * @author Daniel Nogueira
+ * Gera vetores de inteiros aleatórios de acordo com o tamanho desejado
+ * @param tamanho Tamanho do vetor a ser gerado
+ */
+
+    int *gerarVetor(int tamanho) {
+        int *vetor = (int *) malloc(tamanho * sizeof(int));
+        for (int i = 0; i < tamanho; i++) {
+            vetor[i] = rand();
+        }
+        return vetor;
+    }
